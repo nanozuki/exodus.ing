4aaac682a5932269c7daba9d135ff37abd38f8d5
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-07-09 15:24:55 +0800

    feat: init svelte project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div class="title">
+	<img src="/icon.svg" alt="logo" />
+	<h1>Exodusing</h1>
+</div>
+<p>Articles with friends.</p>
+
+<style>
+	.title {
+		display: flex;
+		column-gap: 1rem;
+	}
+</style>
