4e5a8cbcdcb06fbf076319124cae77b5652468bf
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-07-10 17:06:53 +0800

    feat: set minimal style

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import './styles.css';
+
+	const { children } = $props();
+</script>
+
+<div>
+	{@render children()}
+</div>
+
+<style>
+	div {
+		max-width: 40rem;
+		margin: 0 1.5rem;
+	}
+	@media (min-width: 43rem) {
+		div {
+			margin: 0 auto;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,25 @@
 <div class="title">
-	<img src="/icon.svg" alt="logo" />
+	<div>
+		<svg
+			fill="currentColor"
+			xmlns="http://www.w3.org/2000/svg"
+			width="32"
+			height="32"
+			viewBox="0 0 24 24"
+			><path
+				d="M3 1H1v18h18V1zm14 2v14H3V3zm4 18H5v2h18V5h-2zM15 5H5v2h10zM5 9h10v2H5zm7 4H5v2h7z"
+			/></svg
+		>
+	</div>
 	<h1>Exodusing</h1>
 </div>
-<p>Articles with friends.</p>
+<p>nothing can save you except writing</p>
+<p>by Charles Bukowski</p>
 
 <style>
 	.title {
 		display: flex;
 		column-gap: 1rem;
+		align-items: center;
 	}
 </style>
