732a95f7dbe65f649a4488a031611f9f0577dd31
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-07-12 14:18:32 +0800

    feat: register form

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,23 +1,61 @@
 <script lang="ts">
-	let showPassword = $state(false);
-	function togglePassword() {
-		showPassword = !showPassword;
+	import { enhance } from '$app/forms';
+	import ToggleEye from './ToggleEye.svelte';
+
+	let passwordType = $state<'password' | 'text'>('password');
+	let username = $state('');
+	let email = $state('');
+	let password = $state('');
+	let inviteCode = $state('');
+	let errorMsg = $state<string | undefined>(undefined);
+
+	function onchange(open: boolean) {
+		passwordType = open ? 'text' : 'password';
 	}
-	let passwordType = $derived(showPassword ? 'text' : 'password');
 </script>
 
-<form>
-	<label for="username">Username <input type="text" /> </label>
-	<label for="email">Email <input type="text" /> </label>
-	<label for="password">Password <input type={passwordType} /></label>
-	<button type="button" onclick={togglePassword}>View Password</button>
-	<button type="submit">Register</button>
+<svelte:head>
+	<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
+</svelte:head>
+
+<form method="POST" use:enhance>
+	<label for="username">用户名</label>
+	<input name="username" type="text" bind:value={username} required />
+	<label for="email">邮箱地址</label>
+	<input name="email" type="text" bind:value={email} required />
+	<label for="password">密码</label>
+	<input name="password" type={passwordType} bind:value={password} required />
+	<ToggleEye init={false} {onchange} />
+	<label for="inviteCode">邀请码</label>
+	<input name="inviteCode" type="text" bind:value={inviteCode} required />
+	<small class:error={errorMsg}>
+		{errorMsg || '用户名至少三位，可包含小写字母、数字、“-”和“_”，且以小写字母开头'}
+	</small>
+	<div class="cf-turnstile" data-sitekey="1x00000000000000000000AA"></div>
+	<button id="register" class="positive" type="submit">Register</button>
 </form>
 
 <style>
 	form {
-		display: flex;
-		flex-direction: column;
-		row-gap: 1rem;
+		display: grid;
+		grid-template: repeat(5, max-content) / max-content 1fr 2rem;
+		gap: 0.5rem;
+		padding: 2rem 0;
+		align-items: center;
+		max-width: 24rem;
+	}
+	form input {
+		grid-column: 2 / span 2;
+	}
+	form input[name='password'] {
+		grid-column: 2 / span 1;
+	}
+	button#register,
+	small,
+	div.cf-turnstile {
+		grid-column: 1 / span 3;
+	}
+	small.error {
+		color: var(--red);
 	}
 </style>
