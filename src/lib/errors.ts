0375019468280861e1a5817cadd138abb95f52cd
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-07-17 17:30:05 +0800

    feat improve log

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,23 +1,31 @@
-export const InvalidInviteCode = (context?: string): App.Error => ({
-	key: 'INVALID_INVITE_CODE',
-	message: '邀请码无效',
-	context,
-});
+export const InvalidInviteCode = (context?: string): App.Error => {
+	const error = { key: 'INVALID_INVITE_CODE', message: '邀请码无效', context };
+	console.log(error);
+	return error;
+};
 
-export const InviteCodeMissed = (context?: string): App.Error => ({
-	key: 'INVITE_CODE_MISSED',
-	message: '邀请码不能为空',
-	context,
-});
+export const InviteCodeMissed = (context?: string): App.Error => {
+	const error = { key: 'INVITE_CODE_MISSED', message: '邀请码不能为空', context };
+	console.log(error);
+	return error;
+};
 
-export const OAuthValidationError = (context?: string): App.Error => ({
-	key: 'OAUTH_VALIDATION_ERROR',
-	message: 'OAuth 验证错误',
-	context,
-});
+export const OAuthValidationError = (context?: string): App.Error => {
+	const error = {
+		key: 'OAUTH_VALIDATION_ERROR',
+		message: 'OAuth 验证错误',
+		context,
+	};
+	console.log(error);
+	return error;
+};
 
-export const InternalServerError = (context?: string): App.Error => ({
-	key: 'INTERNAL_SERVER_ERROR',
-	message: '服务器内部错误',
-	context,
-});
+export const InternalServerError = (context?: string): App.Error => {
+	const error = {
+		key: 'INTERNAL_SERVER_ERROR',
+		message: '服务器内部错误',
+		context,
+	};
+	console.log(error);
+	return error;
+};
