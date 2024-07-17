9d116ef1cd2fb8c52dbd03f14c2c952651bc6666
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-07-17 16:03:26 +0800

    fix: remove unused file

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,22 +1,3 @@
-/*
-import { getService } from '$lib/server';
-
-export async function load({ cookies }) {
-  const service = getService();
-  const [ceremonies, voter, invited] = await Promise.all([
-    service.getCeremonies(),
-    service.getVoterToken(cookies),
-    service.getInvitedToken(cookies),
-  ]);
-  return {
-    now: new Date(),
-    ceremonies,
-    voter,
-    invited,
-  };
-}
-*/
-
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
