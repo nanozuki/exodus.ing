d1a7c913392ca879bc2d01293e5dfeed4d4a697c
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-07-17 17:02:42 +0800

    feat: improve auth page

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -5,11 +5,10 @@
 </script>
 
 <h2>注册</h2>
-<p>获得邀请方可注册。</p>
+<p class:error={form?.error.inviteCode}>{form?.error.inviteCode || '获得邀请方可注册。'}</p>
 <form id="register" method="POST" action="?/register" use:enhance>
 	<label for="inviteCode">邀请码</label>
 	<input name="inviteCode" type="text" bind:value={inviteCode} required />
-	<small class="error">{form?.error.inviteCode}</small>
 	<button id="register" class="positive" type="submit">使用 GitHub 注册</button>
 </form>
 
@@ -25,13 +24,15 @@
 		margin: 1rem 0;
 		border-top: 1px solid var(--border-color);
 	}
+	p.error {
+		color: var(--red);
+	}
 	form#register {
 		display: grid;
 		grid-template:
 			'label input' auto
-			'error error' auto
 			'submit submit' auto / auto 1fr;
-		gap: 0.5rem;
+		gap: 1rem 0.5rem;
 		align-items: center;
 	}
 	form#register label {
@@ -39,10 +40,6 @@
 	}
 	form#register input {
 		grid-area: input;
-	}
-	form#register small.error {
-		grid-area: error;
-		color: var(--red);
 	}
 	form#register button {
 		grid-area: submit;
