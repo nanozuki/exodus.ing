68b1bc04ee3e385ba1f34ed8864845cfb5aebed4
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-10-21 14:24:18 +0800

    Revert "fix: page in article lists"
    This reverts commit cd6dcedc8a5d7732bee3791673d5abfdd56b081c.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,7 @@ import { listArticles } from '$lib/server/article';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
-  const articles = await listArticles(locals, 100, 0);
+  const articles = await listArticles(locals, 10, 0);
   return {
     articles: articles,
   };
