0bc593f4639e5ee35cb1843b7d2abe40745938f3
Nanozuki Crows <nanozuki.crows@gmail.com>
2025-05-10 11:00:49 +0200

    feat: use node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,10 @@
 import { preprocessMeltUI, sequence } from '@melt-ui/pp';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config}*/
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
-  kit: {
-    // See https://kit.svelte.dev/docs/adapter-cloudflare
-    adapter: adapter({
-      config: 'wrangler.toml',
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>'],
-      },
-      platformProxy: {
-        configPath: 'wrangler.toml',
-        environment: undefined,
-        experimentalJsonConfig: false,
-        persist: true,
-      },
-    }),
-  },
+  kit: { adapter: adapter() },
 };
+
 export default config;
