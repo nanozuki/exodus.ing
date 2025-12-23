b63bc8b762dd7972260fb8f30e1c30537db2791d
Nanozuki Crows <nanozuki.crows@gmail.com>
2025-12-23 22:43:19 +0100

    feat: deploy in fly.io

--- flake.nix
+++ flake.nix
@@ -26,6 +26,7 @@
         {
           devShells.default = pkgs.mkShell {
             packages = with pkgs; [
+              flyctl
               git
               just
               nodePackages."@tailwindcss/language-server"
